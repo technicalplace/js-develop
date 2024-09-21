fad0b56546383df86a4c1e0f89e3b3f0bd6f8547
Shunsuke Fujinawa <shun26497204@gmail.com>
2024-09-21 23:33:15 +0900

    defaultExport Importの回答記載

--- src/lesson/esModules/defaultExport.js
+++ src/lesson/esModules/defaultExport.js
@@ -16,3 +16,5 @@
 function defaultFn() {
   return 'default export';
 }
+
+export default defaultFn;
--- src/lesson/esModules/defaultImport.js
+++ src/lesson/esModules/defaultImport.js
@@ -13,3 +13,5 @@
 
 // 練習問題
 // 問題 2
+import defaultFn from "./defaultExport.js";
+console.log(defaultFn());
