f0f877dfc9e84583fb2150dc1e5d9165bdf0d059
Shunsuke Fujinawa <shun26497204@gmail.com>
2024-09-21 22:44:20 +0900

    Arrayメソッドの回答記載

--- src/lesson/arrayMethods.js
+++ src/lesson/arrayMethods.js
@@ -26,3 +26,27 @@ const array = [
     className: 'piyo',
   },
 ];
+
+// 1通り目
+const newArray = array.map(({ tag, className: prevClassName }) => {
+  return {
+    tag,
+    className: `c-${prevClassName}`
+  }
+})
+
+// 2通り目
+// const newArray = array.map((value) => {
+//   return {
+//     ...value,
+//     className: `c-${value.className}`
+//   }
+// })
+
+// 3通り目
+// const newArray = array.map(({tag, className: prevClassName}) => ({
+//     tag,
+//     className: `c-${prevClassName}`
+// }))
+
+console.log(newArray);
