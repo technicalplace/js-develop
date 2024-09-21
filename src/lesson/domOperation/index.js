62254c0c9e167fd2dd58832cc65deb40aea9587b
Shunsuke Fujinawa <shun26497204@gmail.com>
2024-09-22 00:31:12 +0900

    DOM操作の回答記載

--- src/lesson/domOperation/index.js
+++ src/lesson/domOperation/index.js
@@ -1,13 +1,32 @@
 import 'animate.css';
 
 // ここにコードを書きながら確認しましょう！
+const title = document.getElementById('title');
+const button = document.querySelector('button');
+// const nodeType = document.querySelectorAll('.card-type')[0];
+// const cardEvent = document.querySelectorAll('.card-type')[1];
+const articleNode = document.getElementsByClassName('card-type')
 
+button.addEventListener('click', () => {
+  title.classList.add('animate__hinge');
 
+  setTimeout(() => {
+    title.classList.remove('animate__hinge');
+    // nodeType.classList.add('card-animation');
+    // cardEvent.style.display = 'block';
+    // cardEvent.classList.add('animate__fadeInUp');
+    Array.from(articleNode).forEach(element => {
+      const className = element.getAttribute('class');
 
-
-
-
-
+      if (className.includes('card-type--mocha')) {
+        element.classList.add('card-animation');
+      } else if (className.includes('card-type--yellow')) {
+        element.style.display = 'block';
+        element.classList.add('animate__fadeInUp');
+      }
+    });
+  }, 2000)
+})
 
 
 
