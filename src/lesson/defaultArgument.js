b83fa64c9c83663b98f7296058af66f194c7821e
Shunsuke Fujinawa <shun26497204@gmail.com>
2024-09-21 21:06:09 +0900

    デフォルト引数の問題回答

--- src/lesson/defaultArgument.js
+++ src/lesson/defaultArgument.js
@@ -1,5 +1,10 @@
 // ここにコードを書きながら確認しましょう！
+function discountRateCalc(price, rate = 5) {
+  return price * (1 - (rate * 0.01))
+}
 
+console.log(discountRateCalc(4500));
+console.log(discountRateCalc(6000, 20));
 
 
 
@@ -13,6 +18,6 @@
 
 // 練習問題
 // 問題 1
-function discountRateCalc(price, rate) {
-  return price * (1 - rate * 0.01);
-}
+// function discountRateCalc(price, rate) {
+//   return price * (1 - rate * 0.01);
+// }
