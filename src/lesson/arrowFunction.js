fddcf68a63e9a5f540c52d80c1eb9c08cf68fbd2
Shunsuke Fujinawa <shun26497204@gmail.com>
2024-09-21 22:20:02 +0900

    アロー関数の回答記載

--- src/lesson/arrowFunction.js
+++ src/lesson/arrowFunction.js
@@ -13,6 +13,9 @@
 
 // 練習問題
 // 問題 1
-const testFn = function(name) {
-  return `Gizumo ${name}`;
-};
+// const testFn = function(name) {
+//   return `Gizumo ${name}`;
+// };
+
+const testFn = (name) => `Gizumo ${name}`;
+console.log(testFn('Inc'));
