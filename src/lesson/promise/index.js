6d9099e2fedad4ef1a344a79f6b30f1769c121ee
Shunsuke Fujinawa <shun26497204@gmail.com>
2024-09-23 17:16:21 +0900

    Promiseの回答記載

--- src/lesson/promise/index.js
+++ src/lesson/promise/index.js
@@ -13,3 +13,14 @@
 
 // 練習問題
 // 問題 1
+const returnPromise = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject('エラーが発生しました');
+    }, 3000);
+  });
+};
+
+returnPromise().catch((err) => {
+  console.log(err);
+});
