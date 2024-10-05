2e9bdf72b2c72198afe52c4c248d67f2715160b7
Shunsuke Fujinawa <shun26497204@gmail.com>
2024-10-05 18:03:03 +0900

    アニメーションでカードを追加する処理を2種類記載

--- src/lesson/domOperation/index.js
+++ src/lesson/domOperation/index.js
@@ -3,21 +3,20 @@ import 'animate.css';
 // ここにコードを書きながら確認しましょう！
 const title = document.getElementById('title');
 const button = document.querySelector('button');
-// const nodeType = document.querySelectorAll('.card-type')[0];
-// const cardEvent = document.querySelectorAll('.card-type')[1];
-const articleNode = document.getElementsByClassName('card-type')
+const articleNode = document.querySelectorAll('.card-type')
 
 button.addEventListener('click', () => {
   title.classList.add('animate__hinge');
-
   setTimeout(() => {
     title.classList.remove('animate__hinge');
-    // nodeType.classList.add('card-animation');
-    // cardEvent.style.display = 'block';
-    // cardEvent.classList.add('animate__fadeInUp');
+    articleNode[0].classList.add('card-animation');
+    articleNode[1].style.display = 'block';
+    articleNode[1].classList.add('animate__fadeInUp');
+
+    
+    /** カードが増えていった場合を想定 */
     Array.from(articleNode).forEach(element => {
       const className = element.getAttribute('class');
-
       if (className.includes('card-type--mocha')) {
         element.classList.add('card-animation');
       } else if (className.includes('card-type--yellow')) {
